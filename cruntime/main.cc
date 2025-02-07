598a8bb49510d526811271e7a3444585b5d98769
Dan Green <danngreen1@gmail.com>
2025-02-06 17:18:31 -0800

    Put .data section in ROM to avoid large binary file

--- cruntime/main.cc
+++ cruntime/main.cc
@@ -3,9 +3,14 @@
 auto UART_THR = reinterpret_cast<volatile uint32_t *>(0xfe660000);
 
 int main() {
+  *UART_THR = '\n';
   *UART_THR = 'H';
   *UART_THR = 'e';
   *UART_THR = 'l';
   *UART_THR = 'l';
   *UART_THR = 'o';
+  *UART_THR = '\n';
+
+  while (1)
+    ;
 }
