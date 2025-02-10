b6b6a8a51c5e67b99a457d0662509406b2c642f6
Dan Green <danngreen1@gmail.com>
2025-02-10 14:23:07 -0800

    WIP: interrupt project

---
+++ drivers/rk_gic.h
@@ -0,0 +1,9 @@
+#define GIC_BASE 0xfd400000
+#define GIC_INT_MAX 1023
+#define GIC_PRIO_MAX 32
+#define GIC_INTNO_SGI0 16
+#define GIC_INTNO_PPI0 16
+#define GIC_INTNO_SPI0 320
+
+#define GIC_PRI_SHIFT 5
+#define GIC_PRI_MASK (0x1f)
