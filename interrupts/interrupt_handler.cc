5c810a5bb13147ce0c78d39e389f0c735c51b337
Dan Green <danngreen1@gmail.com>
2025-02-19 09:59:36 -0800

    Move interrupt_handler.cc to mdrivlib

--- interrupts/interrupt_handler.cc
+++
@@ -1,8 +0,0 @@
-#include "drivers/interrupt.hh"
-#include <cstdio>
-
-extern "C" void __attribute__((used)) ISRHandler(unsigned irqnum) {
-
-	printf("IRQ %u\n", irqnum);
-	mdrivlib::InterruptManager::callISR(irqnum);
-}
