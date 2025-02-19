baee54e6d690919fad88e031bd59a422e40f794f
Dan Green <danngreen1@gmail.com>
2025-02-19 09:19:01 -0800

    Cleanup interrupt project main

--- interrupts/main.cc
+++ interrupts/main.cc
@@ -12,52 +12,20 @@ bool BIT(auto reg, unsigned pos) {
 	return (reg & (1 << pos)) ? true : false;
 }
 
+void dump_sys_state();
+
 int main() {
 	// asm(".equ UART_THR, 0xfe660000\nldr x0, =UART_THR\n mov x1, #70\n str x1, [x0]\n");
+	dump_sys_state();
 
 	using namespace RockchipPeriph;
 
-	// Dump some useful info:
-	auto el = get_current_el();
-	printf("\nCurrent EL: %d\n", el);
-
-	auto hcr = read_hcr_el2();
-	printf("HCR = %lx: IMO = %u, FMO = %u\n", hcr, BIT(hcr, 4), BIT(hcr, 3));
-
-	printf("VBAR_EL2 = %08lx\n", read_vbar_el2());
-	printf("DAIF = %08x\n", read_daif());
-	auto spsr = read_spsr_el2();
-	printf("spsr = 0x%lx, IRQ masked = %u, FIQ masked = %u\n", spsr, BIT(spsr, 7), BIT(spsr, 6));
-
 	mdrivlib::IRQ_init();
 
-	/////
-	// mdrivlib::InterruptControl::set_irq_priority(GPIO4IRQ, 1, 0);
-	// mdrivlib::InterruptControl::enable_irq(GPIO4IRQ, mdrivlib::InterruptControl::EdgeTriggered);
 	mdrivlib::InterruptManager::register_and_start_isr(GPIO4IRQ, 1, 0, [] {
-		//
 		printf("GPIO 4 IRQ\n");
 		HW::GPIO4->intr_eoi_H = Gpio::masked_set_bit(Gpio::C(0));
-		//
 	});
-
-	// auto type = InterruptConfig::Edge;
-	// printf("Config GPIO4 as %s\n", type == InterruptConfig::Level ? "level" : "edge");
-	// GIC_SetConfiguration(GPIO4IRQ, type);
-
-	// printf("Disable IRQ %u\n", GPIO4IRQ);
-	// GIC_DisableIRQ(GPIO4IRQ);
-
-	// auto pri = 1 << 0;
-	// printf("Set priority for IRQ %u to %u\n", GPIO4IRQ, pri);
-	// GIC_SetPriority(GPIO4IRQ, pri);
-
-	// printf("Clear pending IRQ %u:\n", GPIO4IRQ);
-	// GIC_ClearPendingIRQ(GPIO4IRQ);
-
-	// printf("Enable IRQ %u:\n", GPIO4IRQ);
-	// GIC_EnableIRQ(GPIO4IRQ);
-	/////
 
 	// Set up GPIO0_C5 as output
 	HW::GPIO0->dir_H = Gpio::masked_set_bit(Gpio::C(5));
@@ -120,4 +88,19 @@ int main() {
 		// 	HW::GPIO4->intr_eoi_H = Gpio::masked_set_bit(Gpio::C(0));
 		// }
 	}
+}
+
+void dump_sys_state() {
+	// Dump some useful info:
+	auto el = get_current_el();
+	printf("\nCurrent EL: %d\n", el);
+
+	auto hcr = read_hcr_el2();
+	printf("HCR = %lx: IMO = %u, FMO = %u\n", hcr, BIT(hcr, 4), BIT(hcr, 3));
+
+	printf("VBAR_EL2 = %08lx\n", read_vbar_el2());
+	printf("DAIF = %08x\n", read_daif());
+
+	auto spsr = read_spsr_el2();
+	printf("spsr = 0x%lx, IRQ masked = %u, FIQ masked = %u\n", spsr, BIT(spsr, 7), BIT(spsr, 6));
 }
