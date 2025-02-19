c1bd8ba00c3e25efbea83a91b5fcf6b651432f2c
Dan Green <danngreen1@gmail.com>
2025-02-19 10:24:46 -0800

    WIP: nested IRQ

--- interrupts/main.cc
+++ interrupts/main.cc
@@ -2,35 +2,64 @@
 #include "drivers/console.hh"
 #include "drivers/gic.hh"
 #include "drivers/gpio.hh"
+#include "drivers/grf.hh"
 #include "drivers/interrupt.hh"
 #include "drivers/irq_init.hh"
+#include "drivers/pwm.hh"
 #include <cstdio>
 
-constexpr uint32_t GPIO4IRQ = 69;
-
-bool BIT(auto reg, unsigned pos) {
-	return (reg & (1 << pos)) ? true : false;
+extern "C" {
+#include "anchor/console/console.h"
 }
+
+constexpr uint32_t GPIO4IRQ = 69;
+constexpr uint32_t PWM0IRQ = 114;
 
 void dump_sys_state();
 
+CONSOLE_COMMAND_DEF(oneshot, "oneshot", CONSOLE_INT_ARG_DEF(num, "Which PWM to fire a one-shot"));
+static void oneshot_command_handler(const oneshot_args_t *args) {
+	if (args->num == 0)
+		HW::PWM0->chan[0].control = 0x01;
+}
+
+CONSOLE_COMMAND_DEF(st, "status", CONSOLE_INT_ARG_DEF(dummy, "Not used"));
+static void st_command_handler(const st_args_t *args) {
+	printf("GPIO4: en: %u IRQstat:%u intr_status:%x (raw: %x)\n",
+		   GIC_GetEnableIRQ(GPIO4IRQ),
+		   GIC_GetIRQStatus(GPIO4IRQ),
+		   HW::GPIO4->intr_status,
+		   HW::GPIO4->intr_rawstatus);
+
+	printf("PWM0: en: %u IRQstat:%u int_en:%x intr_status:%x\n",
+		   GIC_GetEnableIRQ(PWM0IRQ),
+		   GIC_GetIRQStatus(PWM0IRQ),
+		   HW::PWM0->int_en,
+		   HW::PWM0->intsts);
+}
+
 int main() {
-	// asm(".equ UART_THR, 0xfe660000\nldr x0, =UART_THR\n mov x1, #70\n str x1, [x0]\n");
+	console_command_register(oneshot);
+	console_command_register(st);
+
 	dump_sys_state();
 
 	using namespace RockchipPeriph;
 
 	mdrivlib::IRQ_init();
 
-	mdrivlib::InterruptManager::register_and_start_isr(GPIO4IRQ, 1, 0, [] {
+	////////////////////////////////////////
+	// GPIO4 C0 interrupt
+
+	double m = 1;
+	mdrivlib::InterruptManager::register_and_start_isr(GPIO4IRQ, 0, 0, [&m] {
 		printf("GPIO 4 IRQ\n");
+		for (int i = 0; i < 5; i++) {
+			m = m * 2.;
+			printf("GPIO4: %g\n", m);
+		}
 		HW::GPIO4->intr_eoi_H = Gpio::masked_set_bit(Gpio::C(0));
 	});
-
-	// Set up GPIO0_C5 as output
-	HW::GPIO0->dir_H = Gpio::masked_set_bit(Gpio::C(5));
-	HW::GPIO0->high(Gpio::Port::C, 5);
-	HW::GPIO0->low(Gpio::Port::C, 5);
 
 	// Set up GPIO4_C0 as input
 	HW::GPIO4->dir_H = Gpio::masked_clr_bit(Gpio::C(0));
@@ -56,10 +85,35 @@ int main() {
 	// enable interrupt
 	HW::GPIO4->intr_en_H = Gpio::masked_set_bit(Gpio::C(0));
 
-	// Enable IRQ
+	////////////////////////////////////////
+	// PWM interrupt
+
+	auto pwm_irqn = 114;
+
+	mdrivlib::InterruptManager::register_and_start_isr(pwm_irqn, 3, 3, [] {
+		printf("PWM IRQ\n");
+		double x = 2.;
+		while ((long)x < 0x10000000) {
+			printf("PWM: %g\n", x);
+			x = x * 1.0005;
+		}
+		// clear:
+		HW::PWM0->intsts = 0x1;
+	});
+
+	HW::PMU->gpio0_b_h.write(Rockchip::GPIO0B_IOMUX_H_SEL_7::PWM0_M0);
+	HW::PWM0->chan[0].period = 0xFFFF;
+	HW::PWM0->chan[0].duty = 2048;
+	HW::PWM0->int_en = 0x1;
+
+	// Enable IRQs
 	enable_irq();
 	printf("\nEnable IRQ, DAIF = %x\n", read_daif());
 
+	// Set up GPIO0_C5 as output
+	HW::GPIO0->dir_H = Gpio::masked_set_bit(Gpio::C(5));
+	HW::GPIO0->high(Gpio::Port::C, 5);
+	HW::GPIO0->low(Gpio::Port::C, 5);
 	HW::GPIO0->high(Gpio::Port::C, 5);
 	printf("Set GPIO0 C5 high\n");
 
@@ -67,27 +121,15 @@ int main() {
 
 	printf("\n");
 
-	unsigned pp = 0;
 	while (true) {
 		Console::process();
 
 		asm("nop");
-
-		if (pp++ >= 4'000'000) {
-			printf("EL: %d, en: %u IRQstat:%u intr_status:%x (%x)\n",
-				   get_current_el(),
-				   GIC_GetEnableIRQ(GPIO4IRQ),
-				   GIC_GetIRQStatus(GPIO4IRQ),
-				   HW::GPIO4->intr_rawstatus,
-				   HW::GPIO4->intr_status);
-			pp = 0;
-		}
-
-		// if (HW::GPIO4->intr_status & (1 << 16)) {
-		// 	printf("Clear\n");
-		// 	HW::GPIO4->intr_eoi_H = Gpio::masked_set_bit(Gpio::C(0));
-		// }
 	}
+}
+
+bool BIT(auto reg, unsigned pos) {
+	return (reg & (1 << pos)) ? true : false;
 }
 
 void dump_sys_state() {
