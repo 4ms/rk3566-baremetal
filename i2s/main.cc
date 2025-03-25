d37227e867595bed3241d969d8e7195726a0e15f
Dan Green <danngreen1@gmail.com>
2025-03-25 13:43:35 -0700

    WIP I2S (not working yet)

--- i2s/main.cc
+++ i2s/main.cc
@@ -1,4 +1,5 @@
 #include "drivers/console.hh"
+#include "drivers/cru.hh"
 #include "drivers/gic.hh"
 #include "drivers/gpio.hh"
 #include "drivers/grf.hh"
@@ -21,11 +22,11 @@ static void pin_command_handler(const pin_args_t *args) {
 }
 
 int main() {
+	printf("\nStarting I2S example\n");
 	console_command_register(pin);
 
 	using namespace RockchipPeriph;
-
-	mdrivlib::IRQ_init();
+	using namespace mdrivlib::RockchipPeriph;
 
 	// Pins:
 	// (22) GPIO3_C6: I2S1_MCLK_M1
@@ -33,22 +34,67 @@ int main() {
 	// (35) GPIO3_D0: I2S1_LRCK_TX_M1
 	// (40) GPIO3_D1: I2S1_SDO0_M1
 	// (38) GPIO3_D2: I2S1_SDI0_M1
-	HW::SYS->gpio3_c_h.write(Rockchip::GPIO3C_IOMUX_H_SEL_6::I2S1_MCLKM1);
-	HW::SYS->gpio3_c_h.write(Rockchip::GPIO3C_IOMUX_H_SEL_7::I2S1_SCLKTXM1);
-	HW::SYS->gpio3_d_l.write(Rockchip::GPIO3D_IOMUX_L_SEL_0::I2S1_LRCKTXM1);
-	HW::SYS->gpio3_d_l.write(Rockchip::GPIO3D_IOMUX_L_SEL_1::I2S1_SDO0M1);
-	HW::SYS->gpio3_d_l.write(Rockchip::GPIO3D_IOMUX_L_SEL_2::I2S1_SDI0M1);
 
-	// TODO: enable clocks:
-	//		clocks = <&cru MCLK_I2S0_8CH_TX>, <&cru MCLK_I2S0_8CH_RX>, <&cru HCLK_I2S0_8CH>;
-	//		clock-names = "mclk_tx", "mclk_rx", "hclk";
+	// Setup I2S
+	HW::I2S1->XFER = 0;
+	HW::I2S1->CLR = 1;
+	while (HW::I2S1->CLR != 0)
+		;
+	HW::I2S1->tdm_tx8_mode();
+	HW::I2S1->master_tx();
 
+	printf("Select TX to MCLK pin\n");
+	GRF_SOC::i2s1_mclk_sel::write(GRF_SOC::con1_i2s1_mclk_sel::i2s1_mclk_tx);
+	GRF_SOC::i2s1_mclk_tx_oe::write(GRF_SOC::con2_i2s1_mclk_oe::from_cru);
+	GRF_SOC::i2s1_mclk_rx_oe::write(GRF_SOC::con2_i2s1_mclk_oe::from_ext_chip);
+
+	// Clocks:
+
+	// Set the clock divider for cpll
+	// TODO: need to calculate this so we hit 48kHz * 256 = 12.288MHz
+	// Estimate cPLL is at 50MHz to start??? /4 is close
+	// We will probably need to use the fractional divider to get this more exact
+	// printf("Set clock divider (reg %08x)\n", Cru::CLKSEL::reg(Cru::CLKSEL::I2S1_tx));
+	Cru::i2s1_8ch_tx_src_div::write(0x13);
+	Cru::i2s1_8ch_rx_src_div::write(0x13);
+
+	// Select the I2S clock source to be gpll
+	// printf("Set I2S clock source\n\r");
+	// wr 0x0FDD2013C bits 9:8 to 01
+	Cru::i2s1_8ch_tx_src_sel::write(Cru::clk_i2s_8ch_src_sel::clk_cpll_mux);
+	Cru::i2s1_8ch_rx_src_sel::write(Cru::clk_i2s_8ch_src_sel::clk_cpll_mux);
+
+	// Select the MCLK source clock to the integral divided clock
+	// printf("Set I2S MCLK clock source\n\r");
+	// wr 0x0FDD2013C bits 11:10 to 00
+	Cru::mclk_i2s1_8ch_tx_sel::write(Cru::mclk_i2s_8ch_sel::clk_i2s_8ch_src);
+	Cru::mclk_i2s1_8ch_rx_sel::write(Cru::mclk_i2s_8ch_sel::clk_i2s_8ch_src);
+
+	// Connect MCLKOUT to I2S1 TX mclk
+	printf("Connect MCLKOUT\n\r");
+	// wr 0x0FDD2013C 0x80000000
+	Cru::i2s1_mclkout_tx_sel::write(Cru::i2s_mclkout_sel::mclk_i2s_8ch);
+	Cru::i2s1_mclkout_rx_sel::write(Cru::i2s_mclkout_sel::xin_osc0_half);
+
+	// Setup DMA
+	// Setup interrupt
 	// mdrivlib::InterruptManager::register_and_start_isr(GPIO4IRQ, 0, 0, [] {
 	// });
 
 	// Enable IRQs
-	enable_irq();
-	printf("\nEnable IRQ\n");
+	// mdrivlib::IRQ_init();
+	// enable_irq();
+	// printf("\nEnable IRQ\n");
+
+	printf("Enabling pins\n\r");
+	HW::SYS->gpio3_c_h.write(Rockchip::GPIO3C_IOMUX_H_SEL_7::I2S1_SCLKTXM1);
+	HW::SYS->gpio3_c_h.write(Rockchip::GPIO3C_IOMUX_H_SEL_6::I2S1_MCLKM1);
+	HW::SYS->gpio3_d_l.write(Rockchip::GPIO3D_IOMUX_L_SEL_1::I2S1_SDO0M1);
+	HW::SYS->gpio3_d_l.write(Rockchip::GPIO3D_IOMUX_L_SEL_2::I2S1_SDI0M1);
+	HW::SYS->gpio3_d_l.write(Rockchip::GPIO3D_IOMUX_L_SEL_0::I2S1_LRCKTXM1);
+
+	printf("Enable XFER\n");
+	HW::I2S1->XFER = 0b11;
 
 	Console::init();
 
